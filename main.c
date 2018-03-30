8842cc50ed4dc54cac6006da1a5e519fda50ff7d
pinebudweiser <pinebudweiser@gmail.com>
2018-03-30 04:48:36 -0400

    aaa

--- main.c
+++ main.c
@@ -1,7 +1,36 @@
 #include <stdio.h>
+#include <stdint.h>
+#include <pcap/pcap.h>
+#include <libnet.h>
+#include <pthread.h>
 
-int main()
+int *get_recover_time(pcap_t* pkDescriptor);
+
+int main(int argc, char** argv)
 {
-    printf("Hello World!\n");
+    struct libnet_802_3_hdr* ETH;
+    struct libnet_arp_hdr* ARP;
+    char* interface;
+
+
+    if (argc != 3)
+    {
+        printf("Usage : send_arp <interface><sender ip><target ip>");
+        return 1;
+    }
+    interface = argv[1];
+
     return 0;
+}
+
+int *get_recover_time(pcap_t* pkDescriptor)
+{
+    char* buf;
+    struct pcap_pkthdr* pktHeader;
+    pcap_next_ex(pkDescriptor, &pktHeader, &buf);
+
+    while(1)
+    {
+
+    }
 }
